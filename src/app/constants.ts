2395d3933d91167dcc2047407cd17b7c4a5d39c0
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-05-15 17:45:14 -0500

    [ADD] constants

---
+++ src/app/constants.ts
@@ -0,0 +1,14 @@
+export const constants = {
+    MODAL:{
+        ICONS:{
+            SUCCESS: {
+                src: 'assets/img/modal/success.png',
+                alt: 'success icon',
+            },
+            ERROR: {
+                src: 'assets/img/modal/error.png',
+                alt: 'error icon'
+            },
+        }
+    }
+}
