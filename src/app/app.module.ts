1d809a3ccf1b9d26c6367e136497ff23f0ae1612
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-05-15 17:50:23 -0500

    [ADD] app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarFormComponent } from './components/car-form/car-form.component';
+import { PicoPlacaTableComponent } from './components/pico-placa-table/pico-placa-table.component';
+import { CustomH1Component } from './shared/custom-h1/custom-h1.component';
+import { CustomModalComponent } from './shared/custom-modal/custom-modal.component';
+import { CloseModalDirective } from './shared/directives/close-modal.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarFormComponent,
+    PicoPlacaTableComponent,
+    CustomH1Component,
+    CustomModalComponent,
+    CloseModalDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
