5440928c9ba4f0e773f69d661a7d0ee9adab1d0b
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-05-20 18:32:43 -0500

    [ADD] circulation form

--- src/app/components/circulation-form/circulation-form.component.ts
+++ src/app/components/circulation-form/circulation-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { CarModel, CheckCirculationModel } from "src/app/models";
+import { PicoPlacaService } from "src/app/services/pico-placa.service";
 
 @Component({
   selector: 'app-circulation-form',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./circulation-form.component.sass']
 })
 export class CirculationFormComponent {
+  @Input() currentCar: CarModel | null = null;
+  checkCirculationModel: CheckCirculationModel;
+  isError: Boolean = false;
 
+  constructor(private _picoPlacaService: PicoPlacaService) { }
+
+  ngOnInit(): void {
+    this._initModel();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { currentCar } = changes;
+    if(currentCar.currentValue){
+      this.checkCirculationModel.plate = currentCar.currentValue.plate
+    }
+  }
+
+  checkCirculation() {
+    this.checkCirculationModel.date = new Date();
+    this._picoPlacaService.checkCirculation(this.checkCirculationModel).subscribe(resp => {
+      console.log(resp)
+    });
+  }
+
+  setError() {
+    this.isError = true;
+  }
+
+  private _initModel() {
+    this.checkCirculationModel = {
+      plate: "",
+      date: null,
+    }
+  }
 }
