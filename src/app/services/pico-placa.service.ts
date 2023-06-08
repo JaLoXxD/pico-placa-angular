60255b2adb274ffc51db1b84cea51e7d317c7ab0
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-06-08 15:56:29 -0500

    [FIX] format

--- src/app/services/pico-placa.service.ts
+++ src/app/services/pico-placa.service.ts
@@ -5,21 +5,21 @@ import { environment } from '../../environments/environment';
 import { CarsListModel, CheckCirculationModel, CheckCirculationResponseModel, SaveCarModel, SaveCarRequest } from '../models';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PicoPlacaService {
 
-  constructor(private _http: HttpClient) {}
+    constructor(private _http: HttpClient) { }
 
-  createCar(body: SaveCarRequest): Observable<SaveCarModel>{
-    return this._http.post<SaveCarModel>(`${environment.apiUrl}/createCar`, body);
-  }
+    createCar(body: SaveCarRequest): Observable<SaveCarModel> {
+        return this._http.post<SaveCarModel>(`${environment.apiUrl}/createCar`, body);
+    }
 
-  getAllCars(): Observable<CarsListModel>{
-    return this._http.get<CarsListModel>(`${environment.apiUrl}/cars`);
-  }
+    getAllCars(): Observable<CarsListModel> {
+        return this._http.get<CarsListModel>(`${environment.apiUrl}/cars`);
+    }
 
-  checkCirculation(body: CheckCirculationModel): Observable<CheckCirculationResponseModel>{
-    return this._http.post<CheckCirculationResponseModel>(`${environment.apiUrl}/checkCar`, body);
-  }
+    checkCirculation(body: CheckCirculationModel): Observable<CheckCirculationResponseModel> {
+        return this._http.post<CheckCirculationResponseModel>(`${environment.apiUrl}/checkCar`, body);
+    }
 }
