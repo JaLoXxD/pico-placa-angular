f480e8f10ca1223d17e76bd15a9250364223afbf
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-07-23 09:10:15 -0500

    [ADD] pagination

--- src/app/components/cars-table/cars-table.component.ts
+++ src/app/components/cars-table/cars-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { CarModel } from "src/app/models";
+import { CarModel, CarsListModel } from "src/app/models";
 import { PicoPlacaService } from "src/app/services/pico-placa.service";
+import { PaginationModel, PaginatorModel } from '../../models/general';
 
 @Component({
   selector: 'app-cars-table',
@@ -10,18 +11,67 @@ import { PicoPlacaService } from "src/app/services/pico-placa.service";
 export class CarsTableComponent {
   @Output() onSelectCar = new EventEmitter<CarModel>();
   cars: CarModel[]
+  pagination: PaginationModel;
+  paginator: PaginatorModel;
+  carFilter: string;
 
   constructor(private _picoPlacaService: PicoPlacaService){}
 
   ngOnInit(): void {
-    this.getCars();
+    this._getCarsByPage(1);
   }
 
-  getCars(){
-    this._picoPlacaService.getAllCars().subscribe(resp => {
+  private _buildPagination(page: number, limit: number): any {
+    this.pagination = {
+      page,
+      limit
+    }
+  }
+
+  private _buildPaginator(carListModel: CarsListModel): void {
+    const { page, limit, totalDocs, totalPages, nextPage, prevPage } = carListModel;
+    this.paginator = {
+      page,
+      limit,
+      totalDocs,
+      totalPages,
+      nextPage,
+      prevPage
+    }
+  }
+
+  private _getCars(){
+    this._picoPlacaService.getCars(this.pagination).subscribe(resp => {
       console.log(resp);
       this.cars = resp.cars;
+      this._buildPaginator(resp);
     })
+  }
+
+  private _filterCars(){
+    this._picoPlacaService.filterCars(this.pagination, this.carFilter).subscribe(resp => {
+      console.log(resp);
+      this.cars = resp.cars;
+      this._buildPaginator(resp);
+    });
+  }
+
+  private _getCarsByPage(page: number){
+    this._buildPagination(page, 10);
+    this._getCars();
+  }
+
+  private _getFilteredCarsByPage(page: number){
+    this._buildPagination(page, 10);
+    this._filterCars();
+  }
+
+  fillTable(page: number){
+    if(this.carFilter && this.carFilter != ""){
+      this._getFilteredCarsByPage(page);
+    }else{
+      this._getCarsByPage(page);
+    }
   }
 
   setCar(car: CarModel){
