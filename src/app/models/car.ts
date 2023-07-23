58c161632ca7b093cedf77786c6991d08db15348
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-07-23 09:08:59 -0500

    [UPD] CarModel, CarListModel

--- src/app/models/car.ts
+++ src/app/models/car.ts
@@ -1,15 +1,21 @@
 export interface CarModel {
-    [key: string]: any;
-    id?: String;
-    plate: String;
-    color: String;
-    carModel: String;
-    chasis: String;
-    year: Number;
+  [key: string]: any;
+  id?: String;
+  plate: String;
+  color: String;
+  carModel: String;
+  chasis: String;
+  year: number;
 }
 
 export interface CarsListModel {
-    success: String;
-    message: String;
-    cars: CarModel[];
+  success: String;
+  message: String;
+  cars: CarModel[];
+  limit?: number;
+  page?: number;
+  nextPage?: number;
+  prevPage?: number;
+  totalDocs?: number;
+  totalPages?: number;
 }
