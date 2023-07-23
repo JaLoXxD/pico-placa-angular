05e2fc82979f8a0036b1c1a5d3739be5465f4432
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-07-23 09:09:58 -0500

    [UPD] plate input design

--- src/app/components/circulation-form/circulation-form.component.ts
+++ src/app/components/circulation-form/circulation-form.component.ts
@@ -1,60 +1,84 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { constants } from "src/app/constants";
 import { CarModel, CheckCirculationModel } from "src/app/models";
 import { NotifierService, PicoPlacaService } from "src/app/services";
 
 @Component({
-    selector: 'app-circulation-form',
-    templateUrl: './circulation-form.component.html',
-    styleUrls: ['./circulation-form.component.sass']
+  selector: 'app-circulation-form',
+  templateUrl: './circulation-form.component.html',
+  styleUrls: ['./circulation-form.component.sass']
 })
 export class CirculationFormComponent {
-    @Input() currentCar: CarModel | null = null;
-    checkCirculationModel: CheckCirculationModel;
-    isError: Boolean = false;
-    isValidPlate: Boolean = true;
-    appConstants: typeof constants = constants;
+  @Input() currentCar: CarModel | null = null;
+  @Output() onChangeCar = new EventEmitter<CarModel | null>();
 
-    constructor(private _picoPlacaService: PicoPlacaService, private _notifierService: NotifierService) { }
+  appConstants: typeof constants = constants;
+  checkCirculationModel: CheckCirculationModel;
+  isError: Boolean = false;
+  isValidPlate: Boolean = true;
 
-    ngOnInit(): void {
+  constructor(private _picoPlacaService: PicoPlacaService, private _notifierService: NotifierService) { }
+
+  ngOnInit(): void {
+    this._initModel();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { currentCar } = changes;
+    if (currentCar.currentValue) {
+      this.checkCirculationModel.plate = currentCar.currentValue.plate
+    }
+  }
+
+  checkCirculation() {
+    this.checkCirculationModel.date = new Date();
+    this.checkCirculationModel.plate = this.checkCirculationModel.plate.toUpperCase();
+    this._picoPlacaService.checkCirculation(this.checkCirculationModel).subscribe({
+      next: (resp) => {
+        console.log(resp)
+        this._notifierService.showModal(resp.message, this.appConstants.MODAL.ICONS.SUCCESS);
         this._initModel();
-    }
+        this.onChangeCar.emit(null);
+      },
+      error: (err) => {
+        const { error } = err;
+        this._notifierService.showModal(error.message, this.appConstants.MODAL.ICONS.ERROR);
+      }
+    });
+  }
 
-    ngOnChanges(changes: SimpleChanges): void {
-        const { currentCar } = changes;
-        if (currentCar.currentValue) {
-            this.checkCirculationModel.plate = currentCar.currentValue.plate
-        }
-    }
+  setError() {
+    this.isError = true;
+  }
 
-    checkCirculation() {
-        this.checkCirculationModel.date = new Date();
-        this._picoPlacaService.checkCirculation(this.checkCirculationModel).subscribe({
-            next: (resp) => {
-                console.log(resp)
-                this._notifierService.showModal(resp.message, this.appConstants.MODAL.ICONS.SUCCESS);
-                this._initModel();
-            },
-            error: (err) => {
-                const { error } = err;
-                this._notifierService.showModal(error.message, this.appConstants.MODAL.ICONS.ERROR);
-            }
-        });
+  private _initModel() {
+    this.checkCirculationModel = {
+      plate: "",
+      date: null,
     }
+  }
 
-    setError() {
-        this.isError = true;
-    }
+  checkPlate() {
+    this.isValidPlate = this._picoPlacaService.isValidPlate(this.checkCirculationModel.plate);
+  }
 
-    private _initModel() {
-        this.checkCirculationModel = {
-            plate: "",
-            date: null,
-        }
-    }
+  resetPlateValidator() {
+    this.isValidPlate = true;
+  }
 
-    checkPlate() {
-        this.isValidPlate = this._picoPlacaService.isValidPlate(this.checkCirculationModel.plate);
+  formatCarPlate(newValue: string, input: HTMLInputElement): void {
+    newValue = newValue.replace(/[^a-zA-Z0-9]/g, "");
+    const match = newValue.match(/^([A-Za-z]{3})-?(\d+)$/);
+    const hasNumbers = newValue.match(/^[^0-9]/) ? false : true;
+    if(hasNumbers){
+      input.value = '';
+      return;
     }
+    if (match) {
+      input.value = `${match[1]}-${match[2]}`;
+      
+    } else {
+      input.value = newValue.length <= 3 ? newValue : newValue.substring(0, 3);
+    }
+  }
 }
